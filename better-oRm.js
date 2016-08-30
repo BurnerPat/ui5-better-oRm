94fd096e85d7c8ad937e5c72ae8278938cee0bfc
BurnerPat <burner.patrick@googlemail.com>
2016-08-30 10:54:45 +0200

    # intial commit
    Just the initial commit of the whole thing, still need to provide proper jsDocs.

---
+++ better-oRm.js
@@ -0,0 +1,134 @@
+var BetterORM = function (oRm) {
+    this._oRm = oRm;
+    this._elements = [];
+};
+
+BetterORM.Element = function (owner, parent, tag) {
+    this._owner = owner;
+    this._parent = parent;
+    this._tag = tag;
+
+    this._classes = [];
+    this._attributes = {};
+    this._html = null;
+
+    this._children = [];
+};
+
+BetterORM.prototype.create = function (tag) {
+    var element = new BetterORM.Element(this, this, tag);
+    this._elements.push(element);
+    return element;
+};
+
+BetterORM.prototype.flush = function () {
+    this._elements.forEach(function (e) {
+        e.expand();
+    });
+};
+
+BetterORM.Element.prototype.addClass = function (str) {
+    var self = this;
+    str.split(/\s+/).forEach(function (e) {
+        self._classes.push(e);
+    });
+    return this;
+};
+
+BetterORM.Element.prototype.setClass = function (str) {
+    this._classes = [];
+    return this.addClass(str);
+};
+
+BetterORM.Element.prototype.attr = function (name, value) {
+    if (typeof value === "undefined") {
+        return this._attributes[name] || "";
+    }
+
+    this._attributes[name] = value;
+    return this;
+};
+
+BetterORM.Element.prototype.html = function (str) {
+    if (typeof str === "undefined") {
+        return this._html;
+    }
+
+    this._html = str;
+    return this;
+};
+
+BetterORM.Element.prototype.addControl = function (control) {
+    this._children.push(control);
+    return this;
+};
+
+BetterORM.Element.prototype.create = function (tag) {
+    var element = new BetterORM.Element(this, this._owner, tag);
+    this._children.push(element);
+    return element;
+};
+
+BetterORM.Element.prototype.destroy = function () {
+    var i = this._parent._children.indexOf(this);
+    if (i >= 0) {
+        this._parent._children.splice(i, 1);
+    }
+};
+
+BetterORM.escapeHTML = function (html) {
+    return html;
+};
+
+BetterORM.escapeAttribute = function (attr) {
+    return attr.replace(/"/g, "\\\"");
+};
+
+BetterORM.Element.prototype.expand = function () {
+    var oRm = this._owner._oRm;
+
+    oRm.write("<" + this._tag);
+
+    if (this._classes.length > 0) {
+        var classStr = " class=\"";
+
+        for (var i = 0; i < this._classes.length; i++) {
+            if (i > 0) {
+                classStr += " ";
+            }
+
+            classStr += this._classes[i];
+        }
+
+        classStr += "\"";
+
+        oRm.write(classStr);
+    }
+
+    for (var name in this._attributes) {
+        if (this._attributes.hasOwnProperty(name)) {
+            oRm.write(" " + name + "=\"" + BetterORM.escapeAttribute(this._attributes[name]) + "\"");
+        }
+    }
+
+    if (this._html !== null) {
+        oRm.write(">" + BetterORM.escapeHTML(this._html) + "</" + this._tag + ">");
+    }
+    else if (this._children.length > 0) {
+        oRm.write(">");
+
+        this._children.forEach(function (e) {
+            if (e instanceof sap.ui.core.Control) {
+                oRm.renderControl(e);
+            }
+            else if (typeof e.expand === "function") {
+                e.expand();
+            }
+        });
+
+        oRm.write("</" + this._tag + ">");
+    }
+    else {
+        oRm.write("/>");
+    }
+};
